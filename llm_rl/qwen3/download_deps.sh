d988ac7835402bbb350354835b8a7d5dd7faed95
mystri <hanboyou@huawei.com>
2025-12-19 10:28:51 +0800

    updated downloads

--- llm_rl/qwen3/download_deps.sh
+++ llm_rl/qwen3/download_deps.sh
@@ -5,15 +5,14 @@ HOME_DIR=$(pwd)
 mkdir -p /workspace && cd /workspace
 
 # 下载verl源码
-git clone https://github.com/volcengine/verl.git
+git clone https://gitcode.com/GitHub_Trending/ve/verl.git
 cd verl
 git checkout v0.6.0
-git fetch origin pull/3427/head
 git cherry-pick -n -X theirs 448c6c3
 cd -
 
 # 下载Megatron-LM源码
-git clone https://github.com/NVIDIA/Megatron-LM.git
+git clone https://gitcode.com/GitHub_Trending/me/Megatron-LM.git
 cd Megatron-LM
 git checkout core_v0.12.1
 cd -
@@ -25,13 +24,13 @@ git checkout f6688c61bcfe45243ee5eb34c6f013b1e06eca81
 cd -
 
 # 下载vLLM源码
-git clone https://github.com/vllm-project/vllm.git
+git clone https://gitcode.com/GitHub_Trending/vl/vllm.git
 cd vllm
 git checkout v0.11.0
 cd -
 
 # 下载vLLM-Ascend源码
-git clone https://github.com/vllm-project/vllm-ascend.git
+git clone https://gitcode.com/gh_mirrors/vl/vllm-ascend.git
 cd vllm-ascend
 git checkout v0.11.0rc0
 cd -
